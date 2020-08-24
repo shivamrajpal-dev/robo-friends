8a865cdc82701327d4a8704196467e5509aa36db
shivamrajpal <shivamrajpal5@gmail.com>
2020-08-24 23:24:02 +0530

    Updates

---
+++ precache-manifest.85f3ddcaf20c487263f0d1d225a0ce30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae4afba4b4f412fd8673791d5293fe9e",
+    "url": "/shivamrajpal-dev/robofriends.git/index.html"
+  },
+  {
+    "revision": "53d6cc2b77e303cc88e6",
+    "url": "/shivamrajpal-dev/robofriends.git/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "fa8869d6de29c60fe9a0",
+    "url": "/shivamrajpal-dev/robofriends.git/static/css/main.4ae154d3.chunk.css"
+  },
+  {
+    "revision": "53d6cc2b77e303cc88e6",
+    "url": "/shivamrajpal-dev/robofriends.git/static/js/2.2cdae6f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shivamrajpal-dev/robofriends.git/static/js/2.2cdae6f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa8869d6de29c60fe9a0",
+    "url": "/shivamrajpal-dev/robofriends.git/static/js/main.f6b9456b.chunk.js"
+  },
+  {
+    "revision": "d4175aba7a82bff8f9ca",
+    "url": "/shivamrajpal-dev/robofriends.git/static/js/runtime-main.8d42e5ac.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/shivamrajpal-dev/robofriends.git/precache-manifest.85f3ddcaf20c487263f0d1d225a0ce30.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/shivamrajpal-dev/robofriends.git/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
